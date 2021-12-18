56ec5420015c03fdcc6c9a97890125931947e7eb
Nguyen Vinh <nguyenvinh17898@gmail.com>
2021-12-18 23:41:05 +0700

    finish menu

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-$('.search').hover(function(){
+$('.search').hover(function () {
     $('.search-bar').css("display", 'block');
-},function(){
+}, function () {
     $('.search-bar').css("display", 'none');
+})
+
+$('.account').hover(function () {
+    $('.account ul').css("display", 'block');
+}, function () {
+    $('.account ul').css("display", 'none');
+}, function () {
 })
